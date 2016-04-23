37b0aff9317f41b3c9c518f72607d38585a28da1
mihaylov <tbmihailov@gmail.com>
2016-04-23 18:18:51 +0200

    Refactoring: Feature extraction is moved to external class Added: v2 classifier using hierarchical classification

--- sup_parser_v2_hierarchy.py
+++ sup_parser_v2_hierarchy.py
@@ -23,8 +23,7 @@ import random
 import sys
 from datetime import datetime
 
-import logging #word2vec logging
-
+import logging  # word2vec logging
 
 from sklearn import preprocessing
 
@@ -32,18 +31,18 @@ import validator
 from Common_Utilities import CommonUtilities
 
 import gensim
-from gensim import corpora, models, similarities # used for word2vec
-from gensim.models.word2vec import Word2Vec # used for word2vec
-from gensim.models.doc2vec import Doc2Vec#used for doc2vec
+from gensim import corpora, models, similarities  # used for word2vec
+from gensim.models.word2vec import Word2Vec  # used for word2vec
+from gensim.models.doc2vec import Doc2Vec  # used for doc2vec
 
-import time # used for performance measuring
+import time  # used for performance measuring
 import math
 
-from scipy import spatial # used for similarity calculation
+from scipy import spatial  # used for similarity calculation
 from gensim.models.doc2vec import LabeledSentence
 from gensim.models import Phrases
 
-from gensim import corpora # for dictionary
+from gensim import corpora  # for dictionary
 from gensim.models import LdaModel
 
 # from sklearn.svm import libsvm
@@ -57,11 +56,11 @@ import pickle
 from DiscourseSenseClassification_FeatureExtraction_v1 import DiscourseSenseClassification_FeatureExtraction
 
 
-class DiscourseParser_Sup_v1(object):
+class DiscourseSenseClassifier_Sup_v2_Hierarchical(object):
     """Sample discourse relation sense classifier
     """
 
-    def __init__(self, valid_senses, input_run, input_dataset, output_dir, input_params, input_features,class_mapping
+    def __init__(self, valid_senses, input_run, input_dataset, output_dir, input_params, input_features, class_mapping
                  , scale_range=(-1, 1)):
         self.valid_senses = valid_senses
         self.input_run = input_run
@@ -74,13 +73,73 @@ class DiscourseParser_Sup_v1(object):
 
         pass
 
-    def train_sense(self, input_dataset, word2vec_model, save_model_file_basename, scale_features, save_scale_file_basename):
+    def train_sense(self, input_dataset, word2vec_model, save_model_file_basename, scale_features,
+                    save_scale_file_basename, hierachical_classifier=False):
         class_mapping = self.class_mapping
+
+        # Classes:
+        # 'Temporal.Asynchronous.Precedence',
+        # 'Temporal.Asynchronous.Succession',
+        # 'Temporal.Synchrony',
+        # 'Contingency.Cause.Reason',
+        # 'Contingency.Cause.Result',
+        # 'Contingency.Condition',
+        # 'Comparison.Contrast',
+        # 'Comparison.Concession',
+        # 'Expansion.Conjunction',
+        # 'Expansion.Instantiation',
+        # 'Expansion.Restatement',
+        # 'Expansion.Alternative',
+        # 'Expansion.Alternative.Chosen alternative',
+        # 'Expansion.Exception',
+        # 'EntRel',
+
+        class_tree = {'Expansion':
+                          {'ID': 1,
+                           'SubClasses':
+                               {
+                                   'Expansion.Conjunction': {'ID': 11},
+                                   'Expansion.Instantiation': {'ID': 12},
+                                   'Expansion.Restatement': {'ID': 13},
+                                   'Expansion.Alternative': {'ID': 14},
+                                   'Expansion.Alternative.Chosen alternative': {'ID': 15},
+                                   'Expansion.Exception': {'ID': 16},
+                               }
+                           },
+                      'Temporal':
+                          {'ID': 2,
+                           'SubClasses':
+                               {
+                                   'Temporal.Asynchronous.Precedence': {'ID': 21},
+                                   'Temporal.Asynchronous.Succession': {'ID': 22},
+                                   'Temporal.Synchrony': {'ID': 23},
+                               }
+                           },
+                      'Contingency':
+                          {'ID': 3,
+                           'SubClasses':
+                               {
+                                   'Contingency.Cause.Reason': {'ID': 31},
+                                   'Contingency.Cause.Result': {'ID': 32},
+                                   'Contingency.Condition': {'ID': 33},
+                               }
+                           },
+                      'Comparison':
+                          {'ID': 4,
+                           'SubClasses':
+                               {
+                                   'Comparison.Contrast': {'ID': 41},
+                                   'Comparison.Concession': {'ID': 42},
+                               }
+                           },
+                      'EntRel': {'ID': 5},
+                      }
+
         logging.debug(class_mapping)
         word2vec_index2word_set = set(word2vec_model.index2word)
         model_dir = self.input_run
 
-        relation_file = '%s/relations.json' % input_dataset # with senses to train
+        relation_file = '%s/relations.json' % input_dataset  # with senses to train
         relation_dicts = [json.loads(x) for x in open(relation_file)]
 
         parse_file = '%s/parses.json' % input_dataset
@@ -89,26 +148,26 @@ class DiscourseParser_Sup_v1(object):
         random.seed(10)
 
         clf = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-            degree=3, gamma='auto', kernel='rbf',
-            max_iter=-1, probability=False, random_state=None, shrinking=True,
-            tol=0.001, verbose=False)
+                  degree=3, gamma='auto', kernel='rbf',
+                  max_iter=-1, probability=False, random_state=None, shrinking=True,
+                  tol=0.001, verbose=False)
 
         train_x = []
         train_y = []
         for i, relation_dict in enumerate(relation_dicts):
 
-            curr_features_vec = DiscourseSenseClassification_FeatureExtraction.extract_features_as_vector_from_single_record(\
-                relation_dict=relation_dict,\
-                parse=parse,\
-                word2vec_model=word2vec_model,\
+            curr_features_vec = DiscourseSenseClassification_FeatureExtraction.extract_features_as_vector_from_single_record( \
+                relation_dict=relation_dict, \
+                parse=parse, \
+                word2vec_model=word2vec_model, \
                 word2vec_index2word_set=word2vec_index2word_set)
 
-            if (i+1) % 1000 == 0:
+            if (i + 1) % 1000 == 0:
                 print '%s of %s' % (i, len(relation_dicts))
                 logging.info('%s of %s' % (i, len(relation_dicts)))
-                print '%s features:%s'%(i, curr_features_vec)
+                print '%s features:%s' % (i, curr_features_vec)
 
-            curr_senseses = relation_dict['Sense'] # list of senses example: u'Sense': [u'Contingency.Cause.Reason']
+            curr_senseses = relation_dict['Sense']  # list of senses example: u'Sense': [u'Contingency.Cause.Reason']
             # logging.debug('%s - %s'%(i, curr_senseses))
 
             for curr_sense in curr_senseses:
@@ -116,13 +175,12 @@ class DiscourseParser_Sup_v1(object):
                     class_idx = class_mapping[curr_sense]
                     train_x.append(curr_features_vec)
                     train_y.append(class_idx)
-                #else:
-                #     logging.warn('Sense "%s" is not a valid class. Skip'%(curr_sense))
-
+                    # else:
+                    #     logging.warn('Sense "%s" is not a valid class. Skip'%(curr_sense))
 
         scaler = preprocessing.MinMaxScaler(self.scale_range)
         if scale_features:
-            logging.info('Scaling %s items with %s features..' % (len(train_x),len(train_x[0])))
+            logging.info('Scaling %s items with %s features..' % (len(train_x), len(train_x[0])))
             start = time.time()
             train_x = scaler.fit_transform(train_x)
             end = time.time()
@@ -141,11 +199,12 @@ class DiscourseParser_Sup_v1(object):
         pickle.dump(clf, open(save_model_file_basename, 'wb'))
         logging.info('Model saved to %s' % save_model_file_basename)
 
-    def classify_sense(self, input_dataset, word2vec_model, load_model_file_basename, scale_features, load_scale_file_basename):
+    def classify_sense(self, input_dataset, word2vec_model, load_model_file_basename, scale_features,
+                       load_scale_file_basename, hierachical_classifier=False):
         output_dir = self.output_dir
 
         class_mapping = self.class_mapping
-        class_mapping_id_to_origtext = dict([(value, key) for key,value in class_mapping.iteritems()])
+        class_mapping_id_to_origtext = dict([(value, key) for key, value in class_mapping.iteritems()])
         logging.debug('class_mapping_id_to_origtext:')
         logging.debug(class_mapping_id_to_origtext)
 
@@ -179,13 +238,12 @@ class DiscourseParser_Sup_v1(object):
                 word2vec_model=word2vec_model, \
                 word2vec_index2word_set=word2vec_index2word_set)
 
-
             if len(relation_dict['Connective']['TokenList']) > 0:
                 relation_dict['Type'] = 'Explicit'
             else:
                 relation_dict['Type'] = 'Implicit'
 
-            #sense = valid_senses[random.randint(0, len(valid_senses) - 1)]
+            # sense = valid_senses[random.randint(0, len(valid_senses) - 1)]
 
             if scale_features:
                 curr_features_vec = scaler.transform([curr_features_vec])[0]
@@ -193,24 +251,25 @@ class DiscourseParser_Sup_v1(object):
             sense = clf.predict([curr_features_vec])[0]
             # print 'predicted sense:%s' % sense
 
-            #TO DO classmaping id to original class mapping
+            # TO DO classmaping id to original class mapping
             sense_original = class_mapping_id_to_origtext[sense]
             relation_dict['Sense'] = [sense_original]
 
-            #set output data
+            # set output data
             relation_dict['Arg1']['TokenList'] = \
-                    [x[2] for x in relation_dict['Arg1']['TokenList']]
+                [x[2] for x in relation_dict['Arg1']['TokenList']]
             relation_dict['Arg2']['TokenList'] = \
-                    [x[2] for x in relation_dict['Arg2']['TokenList']]
+                [x[2] for x in relation_dict['Arg2']['TokenList']]
             relation_dict['Connective']['TokenList'] = \
-                    [x[2] for x in relation_dict['Connective']['TokenList']]
+                [x[2] for x in relation_dict['Connective']['TokenList']]
             output.write(json.dumps(relation_dict) + '\n')
 
-            if (i+1) % 1000 == 0:
+            if (i + 1) % 1000 == 0:
                 print '%s of %s' % (i, len(relation_dicts))
                 logging.info('%s of %s' % (i, len(relation_dicts)))
                 print '%s features:%s' % (i, curr_features_vec)
         logging.info('output file written:%s' % output_file)
+
 
 # Set logging info
 logFormatter = logging.Formatter('%(asctime)s [%(threadName)-12.12s]: %(levelname)s : %(message)s')
@@ -227,7 +286,6 @@ logger.addHandler(fileHandler)
 consoleHandler = logging.StreamHandler(sys.stdout)
 consoleHandler.setFormatter(logFormatter)
 logger.addHandler(consoleHandler)
-
 
 # SAMPLE RUN:
 # TRAIN:
@@ -247,9 +305,9 @@ if __name__ == '__main__':
 
     cmd = 'train'
     cmd = CommonUtilities.get_param_value("cmd", sys.argv, cmd)
-    logging.info('cmd:%s'%cmd)
+    logging.info('cmd:%s' % cmd)
 
-    #run name for output params
+    # run name for output params
     run_name = ""
     run_name = CommonUtilities.get_param_value("run_name", sys.argv, run_name)
     if run_name != "":
@@ -263,7 +321,7 @@ if __name__ == '__main__':
     scale_features = CommonUtilities.get_param_value_bool("scale_features", sys.argv, scale_features)
     logging.info('scale_features:{0}'.format(scale_features))
 
-    #w2v/doc2vec params
+    # w2v/doc2vec params
     # word2vec model file
     word2vec_model_file = ""  # "qatarliving\\qatarliving_size400_win10_mincnt10.word2vec.bin"
     word2vec_model_file = CommonUtilities.get_param_value("word2vec_model", sys.argv)
@@ -297,7 +355,6 @@ if __name__ == '__main__':
     word2vec_load_bin = CommonUtilities.get_param_value_bool("word2vec_load_bin", sys.argv, word2vec_load_bin)
     logging.info('word2vec_load_bin:{0}'.format(word2vec_load_bin))
 
-
     # Brown clusters file
     brownclusters_file = ""
     brownclusters_file = CommonUtilities.get_param_value("brownclusters_file", sys.argv, brownclusters_file)
@@ -326,30 +383,32 @@ if __name__ == '__main__':
     class_mapping_file = '%s/%s.classlabels' % (output_dir, run_name)
     CommonUtilities.write_dictionary_to_file(class_mapping, class_mapping_file)
 
-    #RUN PARSER
-    parser = DiscourseParser_Sup_v1(valid_senses=valid_senses, input_run=input_run, input_dataset=input_dataset,\
+    # RUN PARSER
+    parser = DiscourseSenseClassifier_Sup_v2_Hierarchical(valid_senses=valid_senses, input_run=input_run, input_dataset=input_dataset, \
                                     output_dir=output_dir, \
-                                    input_params=None, input_features=None,\
+                                    input_params=None, input_features=None, \
                                     class_mapping=class_mapping)
 
     model_file_basename = '%s/%s_model_' % (input_run, run_name)
     scale_file_basename = '%s/%s_scalerange_' % (input_run, run_name)
     if cmd == 'train':
         logging.info('-----------TRAIN---------------------------------')
-        parser.train_sense(input_dataset=input_dataset, word2vec_model=model, save_model_file_basename=model_file_basename,
+        parser.train_sense(input_dataset=input_dataset, word2vec_model=model,
+                           save_model_file_basename=model_file_basename,
                            scale_features=scale_features, save_scale_file_basename=scale_file_basename)
     elif cmd == 'train-test':
         logging.debug(class_mapping)
-        parser.train_sense(input_dataset=input_dataset, word2vec_model=model, save_model_file_basename=model_file_basename,
+        parser.train_sense(input_dataset=input_dataset, word2vec_model=model,
+                           save_model_file_basename=model_file_basename,
                            scale_features=scale_features, save_scale_file_basename=scale_file_basename)
         logging.info('-------------------------------------------------------------')
-        parser.classify_sense(input_dataset=input_dataset, word2vec_model=model, load_model_file_basename=model_file_basename,
+        parser.classify_sense(input_dataset=input_dataset, word2vec_model=model,
+                              load_model_file_basename=model_file_basename,
                               scale_features=scale_features, load_scale_file_basename=scale_file_basename)
     elif cmd == 'test':
         logging.info('-----------TEST----------------------------------')
-        parser.classify_sense(input_dataset=input_dataset, word2vec_model=model, load_model_file_basename=model_file_basename,
+        parser.classify_sense(input_dataset=input_dataset, word2vec_model=model,
+                              load_model_file_basename=model_file_basename,
                               scale_features=scale_features, load_scale_file_basename=scale_file_basename)
     else:
-        logging.error("command unknown: %s. Either -cmd:train or -cmd:test expected"%(cmd))
-
-
+        logging.error("command unknown: %s. Either -cmd:train or -cmd:test expected" % (cmd))
