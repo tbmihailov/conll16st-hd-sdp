532cf79276fe135c8f82dfcd796743f844519f96
mihaylov <tbmihailov@gmail.com>
2016-05-10 10:53:06 +0200

    Refactoring

--- experiment_cross_conv_v1.py
+++ experiment_cross_conv_v1.py
@@ -15,37 +15,40 @@ conv_iter = sent_len - filter_size +1
 
 c = tf.zeros([batch_size, conv_iter, conv_iter])
 
-def tf_convolve(s1,s2, filter_size):
-    batch_size = tf.shape(s1)[0] # Problem here?
-    conv_iter = tf.shape(s1)[1] - filter_size + 1
-    embedding_size = tf.shape(s1)[2]
-    batches_conv_res = tf.variable(tf.float32, [None, None])
+
+def tf_convolve(s1, s2, filter_size, conv_iter, embedding_size, batch_size):
+
+    # batch_size = tf.shape(s1)[0] # Problem here?
+    # conv_iter = tf.shape(s1)[1] - filter_\ize + 1
+    # embedding_size = tf.shape(s1)[2]
+    batches_conv_res = tf.Variable(tf.zeros([1, conv_iter, conv_iter]))
     for bi in range(0, batch_size):
         batch_s1 = tf.gather(s1, bi)
         batch_s2 = tf.gather(s2, bi)
 
-        conv_res = tf.variable(tf.float32, [None, None])
+        conv_res = tf.Variable(tf.zeros([conv_iter]))
         for i in range(0, conv_iter):
             filter_s1 = tf.slice(batch_s1, i, i+filter_size) # tf.slice
-            conv_res_row = tf.variable(tf.float32, [None, None])
+            conv_res_row = tf.Variable(tf.zeros([1]))
             for j in range(0, conv_iter):
                 filter_s2 = tf.slice(batch_s2, j, j+filter_size)
-                curr_val = tf.variable(tf.float32)
+                curr_val = tf.Variable(0)
                 for k in range(0, filter_size):
                     for l in range(0, embedding_size):
                         curr_val += filter_s1[k][l] * filter_s2[k][l]
-                conv_res_row.append(curr_val)
 
-            conv_res.append(conv_res_row)
+                tf.concat(conv_res_row, curr_val) #  conv_res_row.append(curr_val)
 
-        batches_conv_res.append(conv_res)
+            tf.concat(conv_res, conv_res_row) #  conv_res.append(conv_res_row)
 
-    batches_conv_res
+        tf.concat(batches_conv_res, conv_res)# batches_conv_res.append(conv_res)
 
-batches_conv = tf_convolve(s1, s2)
-batches_conv_res = sess.run(batches_conv)
+    return batches_conv_res
 
-print(batches_conv_res)
+batches_conv = tf_convolve(s1, s2, filter_size, conv_iter, embedding_size, batch_size)
+batches_conv_val = sess.run(batches_conv)
+
+print(batches_conv_val)
 
 
 
