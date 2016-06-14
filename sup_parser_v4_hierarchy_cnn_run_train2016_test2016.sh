ee7a67a42c52f33f9bdf94acdd49cff120a1aeb6
mihaylov <tbmihailov@gmail.com>
2016-06-14 02:53:53 +0200

    Added CNN v4 and v5 test and blind test evaluations

---
+++ sup_parser_v4_hierarchy_cnn_run_train2016_test2016.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# set params
+#input_dataset_train=data/conll16st-en-01-12-16-trial
+#input_dataset_test=data/conll16st-en-01-12-16-trial
+
+input_dataset_train=data/conll16-st-train-en-2016-03-29
+input_dataset_test=data/conll16st-en-03-29-16-test
+
+run_type=svm_cnn
+
+run_name=${run_type}_sup_v4_tr16_test16
+if [ -n "$1" ]
+then
+  run_name=$1
+fi     # $String is null.
+
+
+#output dir for parsing results - used for test operations
+output_dir=output/${run_name}
+mkdir -p ${output_dir}
+
+#model dir where output models are saved after train
+model_dir=models/${run_name}
+rm -rf -- ${model_dir}
+mkdir -p ${model_dir}
+
+scale_features=True
+
+# resources
+# word2vec_model=resources/external/w2v_embeddings/qatarliving_qc_size20_win10_mincnt5_rpl_skip1_phrFalse_2016_02_23.word2vec.bin
+word2vec_model=resources/closed_track/word2vec_google/GoogleNews-vectors-negative300.bin
+
+# word2vec_load_bin=False
+word2vec_load_bin=True # for google pretrained embeddings
+
+log_file=${run_name}_$(date +%y-%m-%d-%H-%M).log
+. sup_parser_v4_hierarchy_cnn_run_partial.sh > ${log_file}
